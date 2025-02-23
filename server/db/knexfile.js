0ba9f775ab8ebeb377b1f58f66037750b89f5e02
samueleagogliati <samueleago@gmail.com>
2025-02-23 03:04:47 +0100

    aggiunte config raspberry

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,11 +1,10 @@
 const knexConfig = {
-    client: 'mysql2',
-    connection: {
-      host: 'localhost',
-      user: 'root',
-      password: 'sassa',
-      database: 'expenses_management'
-    }
-  }
+  client: 'mysql2',
+  connection: {
+    host: '192.168.1.30',
+    user: 'root',
+    password: 'sassa',
+    database: 'expenses_management',
+  },
+}
 export default knexConfig
-  
