1b3d0887763db346a816548cde6ce61a8158bcb3
samueleagogliati <samueleago@gmail.com>
2025-03-22 15:23:14 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5173,
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -6,5 +17,12 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://expenses-server:5001', // URL del tuo container backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Rimuove '/api' se necessario
+      },
+    },
   },
 })
