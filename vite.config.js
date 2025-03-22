f41774dbd225058836d0a2efdf263cb1521ddd05
samueleagogliati <samueleago@gmail.com>
2025-03-22 15:30:58 +0100

    m

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// export default defineConfig({
-//   plugins: [vue()],
-//   server: {
-//     host: '0.0.0.0',
-//     port: 5173,
-//   },
-// })
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -17,12 +6,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    proxy: {
-      '/': {
-        target: 'http://expenses-server:5001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
 })
