656d27e61a1c5d05b7b271bf60b2341d485d6ce3
samueleagogliati <samueleago@gmail.com>
2025-03-22 12:10:30 +0100

    fix knexConfig

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -12,7 +12,7 @@
 const knexConfig = {
   client: 'mysql2',
   connection: {
-    host: process.env.DB_HOST || 'db',
+    host: process.env.DB_HOST || 'expenses-db',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'sassa',
     database: process.env.DB_NAME || 'expenses_management',
